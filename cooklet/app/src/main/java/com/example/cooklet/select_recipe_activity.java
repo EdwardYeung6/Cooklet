963ddebbc5c50b3f6a52e45cbe7f96e62881cb5a
EdwardYeung6 <edward6077@gmail.com>
2021-11-01 20:01:09 -0700

    testing layout

---
+++ cooklet/app/src/main/java/com/example/cooklet/select_recipe_activity.java
@@ -0,0 +1,16 @@
+package com.example.cooklet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cooklet.R;
+
+public class select_recipe_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_recipe);
+    }
+}
