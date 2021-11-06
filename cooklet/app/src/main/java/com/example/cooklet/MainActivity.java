56fe6e3a9bbdaff7e5858d4356dca4be720985e4
EdwardYeung6 <edward6077@gmail.com>
2021-11-05 22:52:33 -0700

    scrolling is working but whit space and top bar cannot remove

--- cooklet/app/src/main/java/com/example/cooklet/MainActivity.java
+++ cooklet/app/src/main/java/com/example/cooklet/MainActivity.java
@@ -15,7 +15,7 @@ import com.example.cooklet.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+   //private ActivityMainBinding binding;
     GridView simpleGrid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
